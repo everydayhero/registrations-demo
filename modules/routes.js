7b45d062b2e7b7efc78cd50842c5eb70c19caefd
Keiran <keiran.oleary@everydayhero.com.au>
2016-05-12 16:52:25 +1000

    Remove api component

--- modules/routes.js
+++ modules/routes.js
@@ -1,8 +1,6 @@
 import '../modules/styles.css'
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
-import { ServerRoute } from 'react-project'
-import hello from './api/hello'
 import App from './components/App'
 import NoMatch from './components/NoMatch'
 import SimpleForm from './components/SimpleForm'
@@ -12,9 +10,6 @@ export default (
     <Route path='/' component={App}>
       <IndexRoute component={SimpleForm} />
     </Route>
-    <ServerRoute path='/api'>
-      <ServerRoute path=':hello' get={hello} />
-    </ServerRoute>
     <Route path='*' status={404} component={NoMatch} />
   </Route>
 )
