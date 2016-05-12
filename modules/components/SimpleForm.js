8ab6252f79e879c8cf3d06b19ad7146f2cb54c4c
Keiran <keiran.oleary@everydayhero.com.au>
2016-05-12 13:04:23 +1000

    Return promise on submit

--- modules/components/SimpleForm.js
+++ modules/components/SimpleForm.js
@@ -7,28 +7,34 @@ import { Provider } from 'react-redux'
 export default React.createClass({
 
   onSubmit(data) {
-    const postData = {
-      name: data.pagename,
-      campaign_id: 'au-6018',
-      expires_at: '2016-10-21',
-      target: '500.50',
-      charity_id: 'au-2510',
-      user_name: data.username,
-      user_email: data.email,
-      user_phone: data.phone
-    }
+    return new Promise((resolve, reject) => {
+      const postData = {
+        name: data.pagename,
+        campaign_id: 'au-6018',
+        expires_at: '2016-10-21',
+        target: '500.50',
+        charity_id: 'au-2510',
+        user_name: data.username,
+        user_email: data.email,
+        user_phone: data.phone
+      }
 
-    fetch('https://everydayhero-staging.com/api/v3/pages?access_token=' + process.env.ACCESS_TOKEN, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(postData)
-    }).then(function (res) {
-      return res.json()
-    }).then(function (json) {
-      location.href = json.activation_url
+      fetch('https://everydayhero-staging.com/api/v3/pages?access_token=' + process.env.ACCESS_TOKEN, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(postData)
+      })
+      .then(function (res) {
+        return res.json()
+      })
+      .then(function (json) {
+        location.href = json.activation_url
+        resolve()
+      })
+      .catch(reject)
     })
   },
 
